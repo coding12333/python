54cd05a3cac07293bd242c43386d56a64c693875
coding12333 <ekasjot0715@gmail.com>
2026-09-04 20:10:03 -0700

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,9 @@
+#Printing a greeting messsage
+print("hello,World")
+# printing my name
+print("my name is alex")
+# printing a fun fact about myself
+# print("i Love playing soccer")
+print("i love soccer")
+#preforming and printing a simple calculation
+print("the result of 3+5is",3+5)
