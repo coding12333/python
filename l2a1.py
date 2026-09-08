98c3e665c4aff6f710b81c9132952a58b8187435
coding12333 <ekasjot0715@gmail.com>
2026-09-07 17:34:17 -0700

    Add files via upload

---
+++ l2a1.py
@@ -0,0 +1,14 @@
+#Print a Sentence
+print("Wlecome to the world of programming!")
+
+#Print a Number
+print("4")
+
+#Next Line after print
+print("Hello Python \n")
+
+#Pass multiple arguements for printing
+print("hello",5)
+
+#End arguement of Print Statement
+print("welcome to ", end="*")
