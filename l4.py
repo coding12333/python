2a9850e2e0b36b3c0ac52a9a41d7a9de002567f3
coding12333 <ekasjot0715@gmail.com>
2026-09-12 18:14:19 -0700

    Update l4.py

---
+++ l4.py
@@ -0,0 +1,27 @@
+field1= 120
+field2= 85
+field3= 150
+field4= 95
+field5= 110
+
+total= field1 + field2 + field3 + field4 + field5  
+print("total", total)
+avg= total/5
+print("avg", avg)
+price= 15
+earnings= total*price
+print("earnings", earnings)
+bags= total//25
+print("bags", bags)
+leftover= total%25
+print("leftover", leftover)
+last_year= 500
+print("Better than last year?", total>500)
+print("Same than last year?", total==500)
+print("Worse than last year?", total<500)
+total += 30
+print("updated total", total)
+total -= 15
+print("updated total", total)
+Recalculatebags= total//25
+print("final bag count", total)
